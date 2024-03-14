5d7c4820fa7f4e879d9ee792a0b1bef09090e9ee
Giovaniavs <gavs2@cin.ufpe.br>
2024-03-14 17:11:48 -0300

    feat(entity): create product entity

---
+++ src/products/infrastructure/persistence/relational/entities/product.entity.ts
@@ -0,0 +1,32 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { EntityRelationalHelper } from 'src/utils/relational-entity-helper';
+
+@Entity()
+export class Product extends EntityRelationalHelper {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column('text')
+  description: string;
+
+  @Column('decimal')
+  price: number;
+
+  @Column('integer')
+  availeble_ammount: number;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
